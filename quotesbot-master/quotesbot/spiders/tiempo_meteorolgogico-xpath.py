f47fe1d7d5f284e48f2319efc6264068acad3466
koki <jomeu90@gmail.com>
2019-10-26 17:52:28 +0200

    Equipos y tiempo meteorológico

---
+++ quotesbot-master/quotesbot/spiders/tiempo_meteorolgogico-xpath.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+class TiempoMeteoSpider(scrapy.Spider):
+    name = 'tiempoMeteo'
+    
+    def start_requests(self):
+        base_url = "https://www.transfermarkt.es/laliga/startseite/wettbewerb/ES1/saison_id/2018/plus/1"
+
+    def parse(self, response):
+
+        resumen = {
+        
+        'tabla': response.xpath('//*[@id="yw1_c0"]').extract_first()
+        }
+        
+        yield resumen
