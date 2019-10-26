7fcf3f66ebfc0042222544159a2279594592485c
koki <jomeu90@gmail.com>
2019-10-26 20:12:30 +0200

    cabecera de equipos

--- quotesbot-master/quotesbot/spiders/equipos-xpath.py
+++ quotesbot-master/quotesbot/spiders/equipos-xpath.py
@@ -6,12 +6,16 @@ class EquiposSpider(scrapy.Spider):
     
     def start_requests(self):
         base_url = "https://www.transfermarkt.es/laliga/startseite/wettbewerb/ES1/saison_id/2018/plus/1"
+        yield scrapy.Request(url=(base_url))
 
     def parse(self, response):
 
-        resumen = {
-        
-        'tabla': response.xpath('//*[@id="yw1_c0"]').extract_first()
+        resumen = {        
+        'club': response.xpath('//*[@id="yw1"]/table/thead/tr[1]/th[1]//text()').extract_first(),
+        'plantilla':response.xpath('//*[@id="yw1"]/table/thead/tr[1]/th[4]//text()').extract_first(),
+        'edad':response.xpath('//*[@id="yw1"]/table/thead/tr[1]/th[5]//text()').extract_first(),
+        'extranjeros':response.xpath('//*[@id="yw1"]/table/thead/tr[1]/th[6]//text()').extract_first(),
+        'valor_total':response.xpath('//*[@id="yw1"]/table/thead/tr[1]/th[11]//text()').extract_first()
         }
         
         yield resumen
