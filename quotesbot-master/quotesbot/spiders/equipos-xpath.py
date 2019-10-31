6ccfda5b74ea4623a90593709cd4d173c484e5a4
koki <jomeu90@gmail.com>
2019-10-31 14:38:29 +0100

    mejorando cositas

--- quotesbot-master/quotesbot/spiders/equipos-xpath.py
+++ quotesbot-master/quotesbot/spiders/equipos-xpath.py
@@ -13,7 +13,7 @@ class EquiposSpider(scrapy.Spider):
         for i in range (1,20):
         	equipos={
         	'nombre': response.xpath('//*[@id="yw1"]/table/tbody/tr['+str(i)+']/td[3]//text()').extract_first(),
-        	'plantilla':response.xpath('//*[@id="yw1"]/table/tbody/tr[1]/td[4]//text()').extract_first(),
+        	'plantilla':response.xpath('//*[@id="yw1"]/table/tbody/tr['+str(i)+']/td[4]//text()').extract_first(),
         	'edad':response.xpath('//*[@id="yw1"]/table/tbody/tr['+str(i)+']/td[5]//text()').extract_first(),
         	'extranjeros':response.xpath('//*[@id="yw1"]/table/tbody/tr['+str(i)+']/td[6]//text()').extract_first(),
         	'valor_total':response.xpath('//*[@id="yw1"]/table/tbody/tr['+str(i)+']/td[11]//text()').extract_first(),
--- quotesbot-master/quotesbot/spiders/jugadores-xpath.py
+++ quotesbot-master/quotesbot/spiders/jugadores-xpath.py
@@ -22,7 +22,7 @@ class JuagadoresSpider(scrapy.Spider):
         num_jugadores = response.xpath('count(//*[@id="yw1"]/table/tbody/tr)').extract_first()
         print ("count ----->>> ", num_jugadores)
 
-        for i in range (1, int(num_jugadores[:-2])+1): #equipo['plantilla']
+        for i in range (1, int(num_jugadores[:-2])+1):
             jugador={
             'dorsal': response.xpath('//*[@id="yw1"]/table/tbody/tr['+str(i)+']/td[1]//text()').extract_first(),
             'nombre':response.xpath('//*[@id="yw1"]/table/tbody/tr['+str(i)+']/td[2]//text()').extract_first(),
@@ -37,27 +37,3 @@ class JuagadoresSpider(scrapy.Spider):
             'valor_mercado':response.xpath('//*[@id="yw1"]/table/tbody/tr['+str(i)+']/td[11]//text()').extract_first()
             }
             yield jugador
-
-        """
-
-        for index, row in  enumerate(response.css('#yw1>table>tbody>tr')[0:1]):
-            #<tr class="odd"><td title="Torwart" class="zentriert rueckennummer bg_Torwart"><div class="rn_nummer">1</div></td><td title="" class="posrela"><table class="inline-table" title=""><tbody><tr><td rowspan="2" class=""><a href="#"><img src="https://tmssl.akamaized.net//images/portrait/small/74857-1476949803.jpg?lm=1476949839" title="Marc-André ter Stegen" alt="Marc-André ter Stegen" class="bilderrahmen-fixed"></a></td><td class="hauptlink"><div class="di nowrap"><span class="hide-for-small"><a class="spielprofil_tooltip tooltipstered" id="74857" href="/marc-andre-ter-stegen/profil/spieler/74857">Marc-André ter Stegen</a></span></div><div class="di nowrap"><span class="show-for-small"><a class="spielprofil_tooltip tooltipstered" id="74857" href="/marc-andre-ter-stegen/profil/spieler/74857">M. ter Stegen</a></span></div></td></tr><tr><td>Portero</td></tr></tbody></table></td><td class="zentriert">30/04/1992 (26)</td><td class="zentriert"><img src="https://tmssl.akamaized.net//images/flagge/verysmall/40.png?lm=1520612525" title="Alemania" alt="Alemania" class="flaggenrahmen"></td><td class="zentriert"><a class="vereinprofil_tooltip tooltipstered" id="131" href="/fc-barcelona/startseite/verein/131"><img src="https://tmssl.akamaized.net//images/wappen/verysmall/131.png?lm=1406739548" title="&nbsp;" alt="FC Barcelona" class=""></a></td><td class="zentriert">1,87 m</td><td class="zentriert">derecho</td><td class="zentriert">01/07/2014</td><td class="zentriert"><a class="vereinprofil_tooltip tooltipstered" id="18" href="/borussia-monchengladbach/startseite/verein/18/saison_id/2014"><img src="https://tmssl.akamaized.net//images/wappen/verysmall/18.png?lm=1483890536" title=": Ablöse 12,00 mill. €" alt="Borussia Mönchengladbach" class=""></a></td><td class="zentriert">30.06.2022</td><td class="rechts hauptlink">60,00 mill. €&nbsp;<span title="Vorheriger Marktwert: 60,00 mill. €" class="icons_sprite grey-block-ten">&nbsp;</span></td></tr>
-
-            tds = row.css("#yw1>table>tbody>tr:nth-child(2)")
-            #print (dir(tds[1]))
-            jugador={
-            'dorsal': tds[0].css("div.rn_nummer ::text").extract_first(),
-            'nombre': tds[1].css("::text").extract_first(),
-            'fecha_nacimiento': tds[5].css("::text").extract_first(),
-            'nacionalidad': tds[6].css("img ::attr(title)").extract_first(),
-            'club_actual': tds[0].css("div.rn_nummer ::text").extract_first(),
-            'altura': tds[0].css("div.rn_nummer ::text").extract_first(),
-            'pie': tds[0].css("div.rn_nummer ::text").extract_first(),
-            'fichado': tds[0].css("div.rn_nummer ::text").extract_first(),
-            'club_anterior': tds[0].css("div.rn_nummer ::text").extract_first(),
-            'contrato_hasta': tds[0].css("div.rn_nummer ::text").extract_first(),
-            'valor_mercado': tds[0].css("div.rn_nummer ::text").extract_first(),
-            }
-
-            print ("Len TDs", len(tds))
-            #print(jugador)"""           
