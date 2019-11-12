327a78e7646bf4eff22b643f2d4ba3417cc55222
koki <jomeu90@gmail.com>
2019-11-12 22:44:03 +0100

    vamos con el tiempo

--- quotesbot-master/quotesbot/spiders/tiempo_meteorolgogico-xpath.py
+++ quotesbot-master/quotesbot/spiders/tiempo_meteorolgogico-xpath.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import datetime
 
 class TiempoMeteoSpider(scrapy.Spider):
-    name = 'tiempoMeteo'
+    name = 'tiempoMeteo-xpath'
     
     def start_requests(self):
-        base_url = "https://www.transfermarkt.es/laliga/startseite/wettbewerb/ES1/saison_id/2018/plus/1"
+        base_url = "https://www.eltiempo.es/naron.html?v=por_hora"
         yield scrapy.Request(url=(base_url))
         
     def parse(self, response):
-
-        resumen = {
-        
-        'tabla': response.xpath('//*[@id="yw1_c0"]').extract_first()
-        }
-        
+       	x = datetime.datetime.now()       	
+       	#num_filas = response.xpath('count(//*[@id="page"]/main/div[4]/div/section[5]/div/div[1])').extract_first()
+       	i=2
+       	resumen ={
+        	'fecha': "%s/%s/%s" % (x.day, x.month, x.year),
+        	'horas': response.xpath('//*[@id="page"]/main/div[4]/div/section[5]/div/div[1]/div['+str(i)+']/div[1]//text()').extract_first(),
+        	'temperatura': response.xpath('//*[@id="page"]/main/div[4]/div/section[5]/div/div[1]/div['+str(i)+']/div[2]/span//text()').extract_first(),
+        	'velocidad_viento': response.xpath('//*[@id="page"]/main/div[4]/div/section[5]/div/div[1]/div['+str(i)+']/div[4]/span//text()').extract_first(),        	
+        	'lluvias': response.xpath('//*[@id="page"]/main/div[4]/div/section[5]/div/div[1]/div['+str(i)+']/div[7]/span[2]//text()').extract_first(),
+        	'nieve': response.xpath('//*[@id="page"]/main/div[4]/div/section[5]/div/div[1]/div['+str(i)+']/div[8]/span[2]//text()').extract_first(),
+        	'nubes': response.xpath('//*[@id="page"]/main/div[4]/div/section[5]/div/div[1]/div['+str(i)+']/div[9]/span[2]//text()').extract_first(),
+        	'tormenta': response.xpath('//*[@id="page"]/main/div[4]/div/section[5]/div/div[1]/div['+str(i)+']/div[10]/span[2]//text()').extract_first().strip(),
+        	'humedad': response.xpath('//*[@id="page"]/main/div[4]/div/section[5]/div/div[1]/div['+str(i)+']/div[12]/span[2]//text()').extract_first(),
+        	'prob_precipitacion': response.xpath('//*[@id="page"]/main/div[4]/div/section[5]/div/div[1]/div['+str(i)+']/div[14]/span[2]//text()').extract_first()
+        }        
         yield resumen
