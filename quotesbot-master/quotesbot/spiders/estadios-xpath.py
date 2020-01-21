b5147356b2d8a8ea0d88353bd727ee7ebc78d7b7
Marcial <mrou-repos-service-account@docker-cloud.sdk>
2020-01-21 08:08:59 +0000

    arreglo en estadios xpath.

--- quotesbot-master/quotesbot/spiders/estadios-xpath.py
+++ quotesbot-master/quotesbot/spiders/estadios-xpath.py
@@ -10,16 +10,10 @@ class EstadioSpider(scrapy.Spider):
         yield scrapy.Request(url=(base_url))
         
     def parse(self, response):
-        resumen={
-            'estadio': response.xpath('//*[@id="site"]/div[2]/div[1]/div/div[2]/div/table/tbody/tr[2]/td[2]/a').extract_first(),
-            'ciudad':response.xpath('//*[@id="site"]/div[2]/div[1]/div/div[2]/div/table/tbody/tr[2]/td[3]').extract_first(),
-            'capacidad':response.xpath('//*[@id="site"]/div[2]/div[1]/div/div[2]/div/table/tbody/tr[2]/td[6]').extract_first()
-            }
-        yield resumen
-                    '''for i in range (2,21):
+        for i in range (2,22):
             resumen={
-            'estadio': response.xpath('//*[@id="site"]/div[2]/div[1]/div/div[2]/div/table/tbody/tr['+str(i)+']/td[2]/a').extract_first(),
-            'ciudad':response.xpath('//*[@id="site"]/div[2]/div[1]/div/div[2]/div/table/tbody/tr['+str(i)+']/td[3]').extract_first(),
-            'capacidad':response.xpath('//*[@id="site"]/div[2]/div[1]/div/div[2]/div/table/tbody/tr['+str(i)+']/td[6]').extract_first()
+            'estadio': response.xpath('//*[@id="site"]/div[2]/div[1]/div/div[2]/div/table/tr['+str(i)+']/td[2]//text()').extract_first(),
+            'ciudad':response.xpath('//*[@id="site"]/div[2]/div[1]/div/div[2]/div/table/tr['+str(i)+']/td[3]//text()').extract_first(),
+            'capacidad':response.xpath('//*[@id="site"]/div[2]/div[1]/div/div[2]/div/table/tr['+str(i)+']/td[6]//text()').extract_first()
             }
-            yield resumen'''     
+            yield resumen
