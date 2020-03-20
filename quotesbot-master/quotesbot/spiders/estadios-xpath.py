616278b5cca4905c8a6229717c097f16f2114241
koki <jomeu90@gmail.com>
2020-03-20 21:34:50 +0100

    jornada y estadio

--- quotesbot-master/quotesbot/spiders/estadios-xpath.py
+++ quotesbot-master/quotesbot/spiders/estadios-xpath.py
@@ -12,29 +12,30 @@ ano= url[indice:indice+4]
 class EstadioSpider(scrapy.Spider):
     name = 'estadios-xpath'
 
-    def obtener_url_tiempo(ciudad):
-        url = {
-        "Sevilla" : "https://www.eltiempo.es/sevilla.html?v=por_hora",
-        "Barcelona" : "https://www.eltiempo.es/barcelona.html?v=por_hora",
-        "Gijón" : "https://www.eltiempo.es/gijon.html?v=por_hora",
-        "Pamplona" : "https://www.eltiempo.es/pamplona.html?v=por_hora",
-        "Las Palmas de Gran Canaria" : "https://www.eltiempo.es/las-palmas-de-gran-canaria.html?v=por_hora",
-        "Villarreal" : "https://www.eltiempo.es/vila-real.html?v=por_hora",
-        "Valencia" : "https://www.eltiempo.es/valencia.html?v=por_hora",
-        "Eibar" : "https://www.eltiempo.es/eibar.html?v=por_hora",
-        "Málaga" : "https://www.eltiempo.es/malaga.html?v=por_hora",
-        "Vitoria-Gasteiz" : "https://www.eltiempo.es/vitoria-gasteiz.html?v=por_hora",
-        "Vigo" : "https://www.eltiempo.es/vigo.html?v=por_hora",
-        "Leganés" : "https://www.eltiempo.es/leganes.html?v=por_hora",
-        "Granada" : "https://www.eltiempo.es/granada.html?v=por_hora",
-        "Cornellà de Llobregat" : "https://www.eltiempo.es/cornella-de-llobregat.html?v=por_hora",
-        "La Coruña" : "https://www.eltiempo.es/a-coruna.html?v=por_hora",
-        "Bilbao" : "https://www.eltiempo.es/bilbao.html?v=por_hora",
-        "Madrid" : "https://www.eltiempo.es/madrid.html?v=por_hora",
-        "Getafe" : "https://www.eltiempo.es/getafe.html?v=por_hora",
-        "Girona" : "https://www.eltiempo.es/girona.html?v=por_hora"
+    def obtener_url_tiempo(self,ciudad):
+        tiempo = {        
+            "Sevilla" : "https://www.eltiempo.es/sevilla.html?v=por_hora",
+            "Barcelona" : "https://www.eltiempo.es/barcelona.html?v=por_hora",
+            "Gijón" : "https://www.eltiempo.es/gijon.html?v=por_hora",
+            "Pamplona" : "https://www.eltiempo.es/pamplona.html?v=por_hora",
+            "Las Palmas de Gran Canaria" : "https://www.eltiempo.es/las-palmas-de-gran-canaria.html?v=por_hora",
+            "Villarreal" : "https://www.eltiempo.es/vila-real.html?v=por_hora",
+            "Valencia" : "https://www.eltiempo.es/valencia.html?v=por_hora",
+            "Eibar" : "https://www.eltiempo.es/eibar.html?v=por_hora",
+            "Málaga" : "https://www.eltiempo.es/malaga.html?v=por_hora",
+            "Vitoria-Gasteiz" : "https://www.eltiempo.es/vitoria-gasteiz.html?v=por_hora",
+            "Vigo" : "https://www.eltiempo.es/vigo.html?v=por_hora",
+            "Leganés" : "https://www.eltiempo.es/leganes.html?v=por_hora",
+            "Granada" : "https://www.eltiempo.es/granada.html?v=por_hora",
+            "Cornellà de Llobregat" : "https://www.eltiempo.es/cornella-de-llobregat.html?v=por_hora",
+            "La Coruña" : "https://www.eltiempo.es/a-coruna.html?v=por_hora",
+            "Bilbao" : "https://www.eltiempo.es/bilbao.html?v=por_hora",
+            "Madrid" : "https://www.eltiempo.es/madrid.html?v=por_hora",
+            "Getafe" : "https://www.eltiempo.es/getafe.html?v=por_hora",
+            "Girona" : "https://www.eltiempo.es/girona.html?v=por_hora",
+            "San Sebastián" : "https://www.eltiempo.es/san-sebastian.html?v=por_hora"            
         }
-        return url.get(ciudad, "Desconocido")
+        return tiempo.get(ciudad,"Ciudad inválida")
     
     def start_requests(self):
         base_url = url
@@ -48,6 +49,6 @@ class EstadioSpider(scrapy.Spider):
             'ano': ano,
             'ciudad':response.xpath('//*[@id="site"]/div[2]/div[1]/div/div[2]/div/table/tbody/tr['+str(i)+']/td[3]//text()').extract_first(),
             'capacidad':response.xpath('//*[@id="site"]/div[2]/div[1]/div/div[2]/div/table/tbody/tr['+str(i)+']/td[6]//text()').extract_first(),
-            'url':  '' #obtener_url_tiempo(ciudad)
+            'url':  self.obtener_url_tiempo(ciudad)
             }
-            yield resumen
+            yield resumen        
--- quotesbot-master/quotesbot/spiders/jornada-xpath.py
+++ quotesbot-master/quotesbot/spiders/jornada-xpath.py
@@ -2,21 +2,30 @@
 import scrapy
 import datetime
 
-url="https://resultados.as.com/resultados/futbol/primera/2016_2017/jornada/regular_a_1/"
-#https://resultados.as.com/resultados/futbol/primera/2017_2018/jornada/regular_a_1/
+url="https://resultados.as.com/resultados/futbol/primera/2016_2017/jornada/regular_a_%d/"
+#https://resultados.as.com/resultados/futbol/primera/2017_2018/jornada/regular_a_%d/
 #https://resultados.as.com/resultados/futbol/primera/2016_2017/jornada/regular_a_1/
 ano= url[52:56]
 
 class JornadaSpider(scrapy.Spider):
     name = 'jornada-xpath'
     
+    def jornada_ano_generator(self):
+        for jorn in range(1,4):
+        ##for jorn in range(1,39):
+            yield (jorn)
+
+    
     def start_requests(self):
         base_url = url
-        yield scrapy.Request(url=(base_url))
+        for jor in self.jornada_ano_generator():
+            yield scrapy.Request(url=(base_url %(jor)), callback=self.parse, meta={'jornada': jor})
+
         
     def parse(self, response):
         for i in range (1,2):
             resumen={
+            'jornada': response.meta['jornada'],
             'equipo_local': response.xpath('/html/body/div[7]/div[2]/div/div[2]/div/div/div/ul/li['+str(i)+']/div[1]/a/span[1]//text()').extract_first(),
             'ano': ano,
             'equipo_visitante':response.xpath('/html/body/div[7]/div[2]/div/div[2]/div/div/div/ul/li['+str(i)+']/div[3]/a/span[2]//text()').extract_first(),
