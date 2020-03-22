de89040c7991c5e65b06ede995280f0be570908f
koki <jomeu90@gmail.com>
2020-03-22 12:01:01 +0100

    conectar python a postgresql

--- quotesbot-master/quotesbot/spiders/conectar.py
+++ quotesbot-master/quotesbot/spiders/conectar.py
@@ -1,10 +1,14 @@
-'''import psycopg2
+'''
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT # <-- ADD THIS LINE
 
 #https://pynative.com/python-postgresql-tutorial/#Python_PostgreSQL_Database_Connection
 #https://towardsdatascience.com/python-and-postgresql-how-to-access-a-postgresql-database-like-a-data-scientist-b5a9c5a0ea43
 
 conn = psycopg2.connect(host="localhost", port = 5432, database="prueba1", user="postgres", password="postgres")
 
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 # Create a cursor object
 cur = conn.cursor()
 
@@ -12,6 +16,11 @@ cur = conn.cursor()
 cur.execute("""SELECT * FROM  tfg.staging_jugador where nombre like '%Messi%' """)
 query_results = cur.fetchall()
 print(query_results)
+
+name_Database   = "prueba2"
+# Create table statement -> https://pythontic.com/database/postgresql/create%20database
+sqlCreateDatabase = "create database "+name_Database+";"
+cur.execute(sqlCreateDatabase)
 
 # Close the cursor and connection to so the server can allocate
 # bandwidth to other requests
