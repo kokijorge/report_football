5adbc6062d80b7a022408c330aba7c2b56879f1c
koki <jomeu90@gmail.com>
2020-03-30 22:10:03 +0200

    Actualizando regex 2017

--- quotesbot-master/nafin/reglas.py
+++ quotesbot-master/nafin/reglas.py
@@ -4,28 +4,9 @@
 import re
 from collections import defaultdict
 
-DEBUG = False
-
-"""
-Match ends, Real Betis 0, Levante 3. -> ejemplo de que no hay puntuacion
-
-Goal!Real Betis 0, Levante 3. José Luis Morales (Levante) right footed shot from the centre of the box to the bottom left corner. Assisted by Raphael Dwamena. -> ejemplo de doble puntuacion 10 para el que mete gol 3 para la asistencia
-
-Foul by Gerard Gumbau (Leganés) -> ejemplo de restar, -2
-
-Attempt missed. Juan Camilo Hernández (Huesca) left footed shot from outside the box misses to the left. Assisted by Gonzalo Melero -> ejemplo de ocacion, 4 al primero 2 al segundo
-
-Attempt missed. Javi Guerra (Rayo Vallecano) right footed shot from the centre of the box is too high following a set piece situation -> ejemplo de puntuacion 2  
-OJO que le estoy dando 4
-
-Corner,Real Madrid. Conceded by Borja García -> ejemplo de puntuacion negativa -1
-
-Manu García (Alavés) wins a free kick in the defensive half -> puntuacion +1
-"""
-
-
 def build_patterns():
 	all = [
+	#2017-2018
 		("GOL", "https://regex101.com/r/iqfjCx/1", r"Goal.+?\.\s(.+)\s\(.+?\.(\sAssisted\sby\s)?(?(2)(((?!with|following)\w+\s?)+))" , (0,10,False), (2,3,True) ),
 		("FALTA", "https://regex101.com/r/q9tZBU/1",  r"Foul by\s(.+)\s\(.+\.", (0,-2,False) ),
 		("CORNER", "https://regex101.com/r/n3QmXH/1", r"Corner.+?Conceded by\s(.+)\.", (0,-1,False) ),
@@ -35,7 +16,10 @@ def build_patterns():
 		## "tiro al palo" https://regex101.com/r/a4FWkt/1
 		## "ocasion manifiesta"  https://regex101.com/r/JXnV7v/1
 		## "amarilla" https://regex101.com/r/xINlXr/1 # no me coge el acento?
-		## "tarjeta roja" https://regex101.com/r/xUJPp2/1
+		## "tarjeta roja" "https://regex101.com/r/xUJPp2/1", r"((\w+?\s)+)\(.+?\) is shown the red card", (0,-6,False))
+		## "penalti conseguido"  https://regex101.com/r/bD0s7T/1
+		## "penalti concedido" https://regex101.com/r/raGpDg/1
+	#2016-2017
 	]
 	result = []
 	for rule in all:
@@ -65,23 +49,17 @@ class Patron:
 	def __str__(self):
 		return "\n{0}\t\tURL: {1}\tREGEX: {2} \n{3} ".format(self.name,self.url, self.regex_raw, "\n".join([str(score) for score in self.scores])) 
 
-	def puntua(self, comment):
-		#if DEBUG:
-		#	print ("scoring: " +comment)
+	def puntua(self, comment):		
 		partial = defaultdict(lambda: 0)
 		result = self.pattern.match(comment)
 		if result:
-			groups = result.groups()
-			#if DEBUG:
-			#	print ("Match : " + str(groups))
+			groups = result.groups()			
 			for score in self.scores:
 				player = groups[score.index]
 				if player: # a veces no hay segundo jugador, por ejemplo no hubo asistencia...
 					points = score.points
-					partial[player.strip()] += points  # no deberia tener que hacer strip si el regex estuviese bien construido
-					#print ("\t{0} : {1} {2}".format(self.name, player,points))
-				elif not score.optional:
-					#petardazo porque no podemos saltarnos un player obligatorio en un patron.
+					partial[player.strip()] += points  # no deberia tener que hacer strip si el regex estuviese bien construido					
+				elif not score.optional:					
 					9/0
 		return dict(partial)
 
--- quotesbot-master/nafin/test.py
+++ quotesbot-master/nafin/test.py
@@ -10,7 +10,7 @@ from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
-#FILE_A_PUNTUAR = "prueba.json"
+#FILE_A_PUNTUAR = "as2016_2017.json"
 FILE_A_PUNTUAR = "as2017_2018.json"
 
 print ("\n\n\nVAMOS a puntuar " + FILE_A_PUNTUAR)
@@ -23,22 +23,18 @@ def puntua_partido(comments):
 
 	final = defaultdict(lambda: 0)
 
-	for line, comment in enumerate(comments):
-		#print ("\n{0}\t{1}".format(line+1, comment.rstrip()))
+	for line, comment in enumerate(comments):		
 		for p in patterns:
 			for key, value in p.puntua(comment).items():
-				final[key] += value
-
-	print ("\nTOTALES")
-	#print (json.dumps(final, sort_keys=True, indent=4, ensure_ascii=False))
+				final[key] += value	
 	return final
 
 jornadas = json.loads(comments)
 total= len(jornadas)
 
 for jornada in jornadas[0:total]:
-	p = jornada['partido']
-	partido =  str(p)
+	p = jornada['partido']	
+	partido = p 
 	min_com = jornada['comentarios']
 	num_com = (len(min_com))
 	a = puntua_partido([min_com['text'] for min_com in min_com[:num_com]])
@@ -49,9 +45,7 @@ for jornada in jornadas[0:total]:
 	conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 	cur = conn.cursor()	
 	for nombre,puntuacion in elementos:	
-		punt = str(puntuacion)		
-		sqlInsert = """INSERT INTO tfg.dim_puntuacion(partido,nombre, puntuacion)VALUES ('"""+partido+"""','"""+nombre+"""', """+punt+""");"""
-		print(sqlInsert)
-		cur.execute(sqlInsert)	
+		print (partido, nombre , puntuacion)		
+		cur.execute("INSERT INTO tfg.dim_puntuacion(partido,nombre, puntuacion) VALUES (%s,%s,%s);", (partido, nombre,puntuacion))
 	cur.close()
 	conn.close()	
