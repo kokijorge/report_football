f661ef98fbd735cb8edc67f185df2dbc7a2cb648
koki <jomeu90@gmail.com>
2020-04-18 11:18:34 +0200

    tocando

--- FutbolReport/static/js/main.js
+++ FutbolReport/static/js/main.js
@@ -2,6 +2,15 @@
     console.log($(this).val());
 });*/
 
+function init() {
+
+$('#id_input').val($("#select_temporada").val());
 $(document).on('change', '#select_temporada', function(event) {
 	$('#id_input').val($("#select_temporada").val());
+});
+
+}
+
+jQuery(document).ready(function(){
+    init();
 });
